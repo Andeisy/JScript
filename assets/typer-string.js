350b61eaf87d927e40c31875995f3cf467461b78
Andeisy <andeisyag@gmail.com>
2022-11-18 08:45:04 -0300

    first commit

---
+++ assets/typer-string.js
@@ -0,0 +1,17 @@
+const texto1 = "olá, mundo!";
+const texto2 = "olá, mundo!";
+const stringDeNumero = "345758";
+
+const citacao = 'o leo disse "oi"';
+
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
