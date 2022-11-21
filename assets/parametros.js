66e21fc8771a8c3102256f7b4c859ec243c1faae
Andeisy <andeisyag@gmail.com>
2022-11-21 11:58:58 -0300

    operadores de funcois

---
+++ assets/parametros.js
@@ -0,0 +1,16 @@
+// parametros de função
+
+//function soma (num1, num2) {
+    //return num1 + num2;
+//}
+
+//function multiplica (num1 = 1, num2 = 2){
+   // return num1 * num2;
+//}
+
+//console.log (multiplica(soma(4, 5)))
+
+function comParametro(param) {
+    console.log(param)
+}
+comParametro()
---
+++ assets/template-string.js
@@ -0,0 +1,9 @@
+const nome = "ju";
+const idade = 2021 - 1991;
+const cidadeDeNascimento = "sao paulo";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci em " + cidadeDeNascimento;
+
+ const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log (apresentacao)
